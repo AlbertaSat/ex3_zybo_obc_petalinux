46affb57fa6d391b3e36db67c6581f753d434dc5
Jacques Fourie <jacques@fourie.ca>
2025-08-13 10:48:01 -0600

    Update .gitignore, enhance build_image.sh for tar file creation, and modify device tree for UART and I2C aliases

--- build_image.sh
+++ build_image.sh
@@ -20,3 +20,19 @@ echo "Packaging boot image"
 petalinux-package --boot --force --fsbl images/linux/zynq_fsbl.elf --fpga images/linux/system.bit --u-boot
 echo "Packaging wic image"
 petalinux-package --wic
+
+# Get the current branch name and hash
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+HASH=$(git rev-parse --short HEAD)
+
+# Check if the working directory is dirty
+if [ -n "$(git status --porcelain)" ]; then
+    DIRTY="_dirty"
+else
+    DIRTY=""
+fi
+
+# Create the tar file
+TAR_NAME="images/zybo_obc_${BRANCH}_${HASH}${DIRTY}.tar.gz"
+echo "Creating tar file: $TAR_NAME"
+tar -czvf "$TAR_NAME" -C images/linux petalinux-sdimage.wic
